dcca32306920c4ecd899afa3df877c9e8f95bf11
MarkusIngerslev <markusolsen2004@gmail.com>
2023-03-22 20:33:56 +0100

    tilføjet grid og dialog

--- data.js
+++ data.js
@@ -3,63 +3,91 @@
 console.log("JavaScript is live!");
 window.addEventListener("load", start);
 
-const Markus = {
-  name: "Ampharos",
-  description:
-    "The bright light on its tail can be seen far away. It has been treasured since ancient times as a beacon.",
-  ability: "Static",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/181.png",
-  footprint:
-    "    https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffc03.deviantart.net%2Ffs71%2Fi%2F2013%2F223%2F1%2F2%2F181_mega_ampharos_by_pklucario-d6houxg.png&f=1&nofb=1&ipt=b6d371401e321b8eb396137f840557a9f72336f76d2badea12a1062a8718ee22&ipo=images",
-  dexindex: 181,
-  type: "Electric",
-  subtype: "None",
-  weaknesses: "Ground",
-  gender: "both ♂ & ♀",
-  weight: 61500,
-  height: 140,
-  generation: 2,
-  spilversion: "Gold, Silver & Crystal",
-  canEvolve: false,
-  statsHP: 6,
-  statsAttack: 5,
-  statsDefence: 5,
-  statsSpecialAttack: 7,
-  statsSpecialDefence: 6,
-  statsSpeed: 4,
-};
-
 function start() {
+  // laver constant for min pokemon
+  const Markus = {
+    name: "Ampharos",
+    description:
+      "The bright light on its tail can be seen far away. It has been treasured since ancient times as a beacon.",
+    ability: "Static",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/181.png",
+    footprint: "🐾",
+    dexindex: 181,
+    type: "Electric",
+    subtype: "None",
+    weaknesses: "Ground",
+    gender: "both ♂ & ♀",
+    weight: 61500,
+    height: 140,
+    generation: 2,
+    spilversion: "Gold, Silver & Crystal",
+    canEvolve: false,
+    statsHP: 6,
+    statsAttack: 5,
+    statsDefence: 5,
+    statsSpecialAttack: 7,
+    statsSpecialDefence: 6,
+    statsSpeed: 4,
+  };
+
+  //viser min pokemon
   showPokemon(Markus);
 }
 
 function showPokemon(pokemon) {
-  const pokemonDialog = /*html */ `
-    <ul>
-        <li>The pokémons name is: ${pokemon.name}</li>
-        <img src="${pokemon.image}">
-        <li>Ability: ${pokemon.ability}</li>
-        <li>${pokemon.description}</li>
-        <li>Index number #${pokemon.dexindex}</li>
-        <li>Type: ${pokemon.type}</li>
-        <li>Subtype: ${pokemon.subtype}</li>
-        <li>Weaknesses: ${pokemon.weaknesses}</li>
-        <li>The pokemon can be: ${pokemon.gender}</li>
-        <li>${pokemon.name}'s' weight is ${pokemon.weight} g</li>
-        <li>${pokemon.name}'s height is ${pokemon.height} cm</li>
-        <li>Generation: ${pokemon.generation}</li>
-        <li>First gameversion: ${pokemon.spilversion}</li>
-        <li>Can ${pokemon.name} evolve further? ${pokemon.canEvolve}</li>
-        <li>HP stat out of 15: ${pokemon.statsHP}</li>
-        <li>Attack stat out of 15: ${pokemon.statsAttack}</li>
-        <li>Defence stat out of 15: ${pokemon.statsDefence}</li>
-        <li>Special attack stat out of 15: ${pokemon.statsSpecialAttack}</li>
-        <li>Special defence stat out of 15: ${pokemon.statsSpecialDefence}</li>
-        <li>Speed stat out of 15: ${pokemon.statsSpeed}</li>
-        <img src="${pokemon.footprint}">
-    </ul>
-  `;
+  document.querySelector("#view").insertAdjacentHTML(
+    "afterbegin",
+    /*html */ `
+    <article>
+      <img src="${pokemon.image}">
+      <h2>${pokemon.name}</h2>
+      <p>Ability: ${pokemon.ability}</p>
+      <p>${pokemon.description}</p>
+      <p>Index number #${pokemon.dexindex}</p>
+      <p>Type: ${pokemon.type}</p>
+      <p>Subtype: ${pokemon.subtype}</p>
+      <p>Weaknesses: ${pokemon.weaknesses}</p>
+      <p>The pokemon can be: ${pokemon.gender}</p>
+      <p>${pokemon.name}'s' weight is ${pokemon.weight} g</p>
+      <p>${pokemon.name}'s height is ${pokemon.height} cm</p>
+      <p>Generation: ${pokemon.generation}</p>
+      <p>First gameversion: ${pokemon.spilversion}</p> 
+    </article>
+  `
+  );
+
+  document.querySelector("#dialog-box").insertAdjacentHTML(
+    "afterbegin",
+    /* html */ `
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.image}">
+    <p>Ability: ${pokemon.ability}</p>
+    <p>${pokemon.description}</p>
+    <p>Index number #${pokemon.dexindex}</p>
+    <p>Type: ${pokemon.type}</p>
+    <p>Subtype: ${pokemon.subtype}</p>
+    <p>Weaknesses: ${pokemon.weaknesses}</p>
+    <p>The pokemon can be: ${pokemon.gender}</p>
+    <p>${pokemon.name}'s' weight is ${pokemon.weight} g</p>
+    <p>${pokemon.name}'s height is ${pokemon.height} cm</p>
+    <p>Generation: ${pokemon.generation}</p>
+    <p>First gameversion: ${pokemon.spilversion}</p>
+    <p>Can ${pokemon.name} evolve further? ${pokemon.canEvolve}</p>
+    <p>HP stat out of 15: ${pokemon.statsHP}</p>
+    <p>Attack stat out of 15: ${pokemon.statsAttack}</p>
+    <p>Defence stat out of 15: ${pokemon.statsDefence}</p>
+    <p>Special attack stat out of 15: ${pokemon.statsSpecialAttack}</p>
+    <p>Special defence stat out of 15: ${pokemon.statsSpecialDefence}</p>
+    <p>Speed stat out of 15: ${pokemon.statsSpeed}</p>
+    <p>Footprint ${pokemon.footprint}</p>
+    `
+  );
+
   document
-    .querySelector("#view")
-    .insertAdjacentHTML("afterbegin", pokemonDialog);
+    .querySelector("#view article:last-child")
+    .addEventListener("click", pokemonClicked);
+  function pokemonClicked() {
+    //show dialog
+    document.querySelector("#dialog-box").showModal();
+  }
 }
